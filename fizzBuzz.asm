110a188e5046b63c4ccbe63ed308d25d95127642
Michael Burke <michael.dburke56@gmail.com>
2022-02-21 21:29:09 -0500

    Initial commit. Implementation is incomplete.

---
+++ fizzBuzz.asm
@@ -0,0 +1,21 @@
+section .text
+    global _start
+  _start:
+        mov ecx, 1        ;set loop counter
+
+        loop_main:
+        push ecx          ;save loop counter
+
+        xor dx, dx        ;reset dx
+        mov ax, cx
+        mov bx, 15
+        div bx
+        cmp dx, 0
+        jz div_by_3_5     ;is divisibe by 3&5
+        
+        xor dx, dx        ;reset dx
+        mov ax, cx
+        mov bx, 3
+        div bx
+        cmp dx, 0
+        jz div_by_3       ;is divisible by 3
