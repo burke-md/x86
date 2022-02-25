2cf25f1a1640642061e840d8d767470fa1d7a558
Michael Burke <michael.dburke56@gmail.com>
2022-02-25 11:21:29 -0500

    Continue writting fizz buzz.

--- fizzBuzz.asm
+++ fizzBuzz.asm
@@ -19,3 +19,13 @@ section .text
         div bx
         cmp dx, 0
         jz div_by_3       ;is divisible by 3
+
+        xor dx, dx        ; reset dx
+        mov ax, cx        
+        mov bx, 5
+        div bx
+        cmp dx, 0
+        jz div_by_5       ;is divisible by 5
+
+        ; print number
+        mov eax, ecx      ;set print_num arg
